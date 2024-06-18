3687131eddae4b8605010e686e1719bc0626ce6e
henriquepablo <pablo269704@gmail.com>
2024-06-18 20:20:00 -0300

    feat: making request to get balance

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,40 @@
-import { Button, Text, View } from "react-native";
+import { useContext, useEffect, useState } from "react";
 
-import { useContext } from "react";
-import { AuthContext } from "../../Contexts/auth";
 import { Background } from "./styles";
 import Header from "../../Components/Header";
+import { format } from "date-fns";
+import api from "../../services/api";
+import { AxiosResponse } from "axios";
 
 const Home = ():React.JSX.Element => {
+
+    const [listBalance, setListBalance] = useState([]);
+
+    const [dateMovents, setDateMovements] = useState(new Date());
+
+    useEffect(() => {
+        let isActive:boolean = true;
+
+        async function getMovements() {
+            let dateFormated:string = format(dateMovents, "dd/MM/yyyy");
+
+            const balance:AxiosResponse<any, any> = await api.get('/balance', {
+                params: {
+                    date: dateFormated
+                }
+            });
+
+            if(isActive) {
+                setListBalance(balance.data);
+            }
+        }
+
+        getMovements();
+
+        return (() => {
+            isActive = false;
+        }); 
+    }, []);
 
     return(
         <Background>
