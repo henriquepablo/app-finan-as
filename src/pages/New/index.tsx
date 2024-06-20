cc0a487328a3db46267451b73bf5b9c101bc85a2
henriquepablo <pablo269704@gmail.com>
2024-06-20 20:19:12 -0300

    feat: Recording Spending

--- src/pages/New/index.tsx
+++ src/pages/New/index.tsx
@@ -1,14 +1,58 @@
-import { Keyboard, SafeAreaView, TouchableWithoutFeedback } from "react-native";
+import { Keyboard, SafeAreaView, TouchableWithoutFeedback, Alert } from "react-native";
 import Header from "../../Components/Header";
 import { Background, Input, SubmitButton, SubmitText } from "./styles";
 import { useState } from "react";
 import RegisterTypes from "../../Components/RegisterTypes";
+import api from "../../services/api";
+import { format } from "date-fns";
+import { useNavigation } from "@react-navigation/native";
 
 const New = ():React.JSX.Element => {
+
+    const navigation = useNavigation();
 
     const [labelInput, setLabelInput] = useState('');
     const [valueInput, setValueInput] = useState('');
     const [type, setType] = useState('receita');
+
+    const handleSumbit = ():void => {
+        Keyboard.dismiss();
+        
+        if (isNaN(parseFloat(valueInput)) || type === null) {
+          Alert.alert('Preencha todos os campos'); 
+          return;
+        }
+
+        Alert.alert(
+            'Confirmando dados',
+            `Tipo ${type} - valor: ${parseFloat(valueInput)}`,
+            [
+                {
+                    text: 'Cancelar',
+                    style: 'cancel'
+                },
+                {
+                    text: 'Continuar',
+                    onPress: () => handleAdd()
+                }
+            ]
+        );
+    }
+
+    async function handleAdd() {
+        Keyboard.dismiss();
+        
+        await api.post('/receive', {
+            description: labelInput,
+            value: Number(valueInput),
+            type: type,
+            date: format(new Date(), 'dd/MM/yyyy')
+        });
+
+        setLabelInput('');
+        setValueInput('');
+        navigation.navigate('Home' as never);
+    }
 
     return(
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -31,7 +75,7 @@ const New = ():React.JSX.Element => {
                     
                     <RegisterTypes type={type} sendTypeChanged={(item:string) => setType(item)}/>
 
-                    <SubmitButton>
+                    <SubmitButton onPress={handleSumbit}>
                         <SubmitText>Registrar</SubmitText>
                     </SubmitButton>
                 </SafeAreaView>
